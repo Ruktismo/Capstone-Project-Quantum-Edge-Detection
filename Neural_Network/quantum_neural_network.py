b7766098447dbfbfbe1c4f57bc11f699d0853ee9
yumit <ylamansk@asu.edu>
2023-08-29 20:41:34 -0700

    test documentation; set up QCNN file

---
+++ Neural_Network/quantum_neural_network.py
@@ -0,0 +1,66 @@
+#tutorial: https://qiskit.org/ecosystem/machine-learning/tutorials/11_quantum_convolutional_neural_networks.html
+#import libraries
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+from IPython.display import clear_output
+from qiskit import QuantumCircuit
+from qiskit.algorithms.optimizers import COBYLA
+from qiskit.circuit import ParameterVector
+from qiskit.circuit.library import ZFeatureMap
+from qiskit.quantum_info import SparsePauliOp
+from qiskit.utils import algorithm_globals
+from qiskit_machine_learning.algorithms.classifiers import NeuralNetworkClassifier
+from qiskit_machine_learning.neural_networks import EstimatorQNN
+from sklearn.model_selection import train_test_split
+
+algorithm_globals.random_seed = 12345
+
+
+
+#Convolutional Layer
+# We now define a two qubit unitary as defined in [3]
+def conv_circuit(params):
+    target = QuantumCircuit(2)
+    target.rz(-np.pi / 2, 1)
+    target.cx(1, 0)
+    target.rz(params[0], 0)
+    target.ry(params[1], 1)
+    target.cx(0, 1)
+    target.ry(params[2], 1)
+    target.cx(1, 0)
+    target.rz(np.pi / 2, 0)
+    return target
+
+
+# Let's draw this circuit and see what it looks like
+params = ParameterVector("θ", length=3)
+circuit = conv_circuit(params)
+circuit.draw("mpl")
+
+def conv_layer(num_qubits, param_prefix):
+    qc = QuantumCircuit(num_qubits, name="Convolutional Layer")
+    qubits = list(range(num_qubits))
+    param_index = 0
+    params = ParameterVector(param_prefix, length=num_qubits * 3)
+    for q1, q2 in zip(qubits[0::2], qubits[1::2]):
+        qc = qc.compose(conv_circuit(params[param_index : (param_index + 3)]), [q1, q2])
+        qc.barrier()
+        param_index += 3
+    for q1, q2 in zip(qubits[1::2], qubits[2::2] + [0]):
+        qc = qc.compose(conv_circuit(params[param_index : (param_index + 3)]), [q1, q2])
+        qc.barrier()
+        param_index += 3
+
+    qc_inst = qc.to_instruction()
+
+    qc = QuantumCircuit(num_qubits)
+    qc.append(qc_inst, qubits)
+    return qc
+
+
+circuit = conv_layer(4, "θ")
+circuit.decompose().draw("mpl")
+
+
+#generate data here
--- Quantum Vision Robot Car Wiki/Quantum/QED.md
+++ Quantum Vision Robot Car Wiki/Quantum/QED.md
@@ -1,3 +1,5 @@
-Quantum Edge Detection ([[QED]]) is used by our [[Quantum]] team to quickly find the edges in an image. Most of the theory and code comes from the [[Qiskit]] Python module. A reference to their paper on it can be found [here](https://learn.qiskit.org/course/ch-applications/quantum-edge-detection).  Note that the paper is a bit old, running on [[Qiskit]]v0.25.4 and we are using [[Qiskit]]v0.41.0. So some parts of the code have changed but the general theory described in the paper still applies.
+Quantum Edge Detection ([[QED]]) is used by our [[Quantum]] team to quickly find the edges in an image. 
+Most of the theory and code comes from the [[Qiskit]] Python module. A reference to their paper on it can be found [here](https://learn.qiskit.org/course/ch-applications/quantum-edge-detection).  
+Note that the paper is a bit old, running on [[Qiskit]]v0.25.4 and we are using [[Qiskit]]v0.41.0. So some parts of the code have changed but the general theory described in the paper still applies.
 
 Below we will be going through some examples using our code.
